3325b37b8b293b26ec475d0644189cf87376f7af
Esteban <55029850+EquirozVasco@users.noreply.github.com>
2022-04-12 18:44:54 -0500

    Se agrega documentación

--- src/services/mongodb.service.js
+++ src/services/mongodb.service.js
@@ -18,12 +18,23 @@ const consultarDocumentos = async (nombreColeccion, filtro) => {
   return coleccion.find(filtro).limit(parseInt(process.env.DEFAULT_LIMIT_PROPERTIES)).toArray();
 }
 
+/*
+Con la constante 'property_type_field' indicamos en project los campos que queremos ver de la BD
+*/
+
 const consultarTipoPropiedad = async (nombreColeccion) => {
   let db = await conectarDB();
   let coleccion = db.collection(nombreColeccion);
   const property_type_field = { _id: 0, property_type: 1 };
   return coleccion.find().project(property_type_field).toArray();
 }
+
+/*
+Con const sort entregamos los parámetros de cómo queremos ordenar la tabla y por cual columna.
+En este caso por number_of_reviews y de forma descendente.
+
+la const limit entrega la cantidad de registros que se van a mostrar en la petición.
+*/
 
 const reviewsTop20 = async (nombreColeccion) => {
   let db = await conectarDB();
@@ -33,6 +44,12 @@ const reviewsTop20 = async (nombreColeccion) => {
   const limit = 20;
   return coleccion.find().project(property_type_field).sort(sort).limit(limit).toArray();
 }
+
+/*
+La función topCamas tiene un comportamiento similar al de 'reviewsTop20', sin embargo en este caso particular
+se recibe un parámetro adicional (limite), este sirve para indicar el número de propiedades que se quieren ver
+ordenadas de forma descendente por la cantidad de camas que tienen. 
+*/
 
 const topCamas = async (nombreColeccion, limite) => {
   let db = await conectarDB();
